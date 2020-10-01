9cc86bdd625f4f7f56b1f083f1487f56d2b624a1
GGULBAE <61654922+GGULBAE@users.noreply.github.com>
2020-10-01 14:53:41 +0900

    workflows

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style_imageWrapper}>
+      <img style={style_imageSVG} src={require("./assets/1.svg")} alt=""></img>
     </div>
   );
 }
 
 export default App;
+
+const style_imageWrapper = {
+  width: "100vw",
+  height: "100vh"
+}
+
+const style_imageSVG = {
+  width: "100%",
+  height: "100%"
+}
