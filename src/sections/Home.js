d0c51145324c8acc4f69af82d52c8bed82144258
GGULBAE <hyeon5727@gmail.com>
2020-10-06 00:04:47 +0900

    for Action

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
 
-var automatic_scroll;
+// var automatic_scroll;
 
-function afterLoad(origin, destination, direction) {
-  console.log("---AFTER LOAD---")
-  console.log(origin, destination, direction);
-  // console.log("After load: " + destination.index);
-  console.log("---AFTER LOAD FINISH---")
+// function afterLoad(origin, destination, direction) {
+//   console.log("---AFTER LOAD---")
+//   console.log(origin, destination, direction);
+//   // console.log("After load: " + destination.index);
+//   console.log("---AFTER LOAD FINISH---")
 
-  if (automatic_scroll)
-    clearInterval(automatic_scroll);
+//   if (automatic_scroll)
+//     clearInterval(automatic_scroll);
   
-  automatic_scroll = setInterval(() => {
-    var target = document.getElementsByClassName("fp-controlArrow fp-next");
-    if (target) {
-      target = target[0];
-    } else {
-      // do nothing
-      return;
-    }
-    target.click();
-  }, 3000)
-}
+//   automatic_scroll = setInterval(() => {
+//     var target = document.getElementsByClassName("fp-controlArrow fp-next");
+//     if (target) {
+//       target = target[0];
+//     } else {
+//       // do nothing
+//       return;
+//     }
+//     target.click();
+//   }, 3000)
+// }
 
 const Fullpage2 = () => (
   <ReactFullpage
@@ -65,11 +65,11 @@ const Fullpage2 = () => (
   />
 );
 
-function Fullpage() {
-  return <div style={style_Main_ImageWrapper}>
-    <embed style={style_Main} src={require("../assets/homepagetemp1.svg")} type="image/svg+xml"></embed>
-  </div>
-}
+// function Fullpage() {
+//   return <div style={style_Main_ImageWrapper}>
+//     <embed style={style_Main} src={require("../assets/homepagetemp1.svg")} type="image/svg+xml"></embed>
+//   </div>
+// }
 
 export default function Home(props) {
     return <React.Fragment>
@@ -77,17 +77,17 @@ export default function Home(props) {
     </React.Fragment>
 }
 
-const style_Main_Section = {
-  // textAlign: "center",
-  // backgroundColor: "#010102"
-}
+// const style_Main_Section = {
+//   // textAlign: "center",
+//   // backgroundColor: "#010102"
+// }
 
-const style_Main_ImageWrapper = {
-  width: "100%",
-  height: "100%",
-  position: "relative",
-  overflow: "hidden"
-}
+// const style_Main_ImageWrapper = {
+//   width: "100%",
+//   height: "100%",
+//   position: "relative",
+//   overflow: "hidden"
+// }
 
 const style_Main = {
   // position: "absolute",
