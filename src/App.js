398816654f86bdd71aece27cb194f11e684c8af3
GGULBAE <hyeon5727@gmail.com>
2020-10-09 23:09:55 +0900

    [Re:]Legacy Delete 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './fonts/bahnschrift-cufonfonts-webfont/style.css';
-
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
