f637aa5c1c949d1fdcc9c4f6f709d25e61d566b4
GGULBAE <hyeon5727@gmail.com>
2020-10-09 23:08:16 +0900

    [Re:]Legacy Delete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './fonts/bahnschrift-cufonfonts-webfont/style.css'; // font import
 import './index.css';
 import App from './App';
 
