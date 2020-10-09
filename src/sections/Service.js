96e0fc03c63cd47f84b6c4d38e22ab0a003687d2
GGULBAE <hyeon5727@gmail.com>
2020-10-10 00:19:46 +0900

    [Re:]Team Legacy

--- src/sections/Service.js
+++ src/sections/Service.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Service(props) {
     // return <img style={serviceWrapper} src={require("../assets/Service.svg")} alt=""></img>
     // return <embed style={serviceWrapper} src={require("../assets/service2.svg")} type="image/svg+xml"></embed>
-    return <span>service</span>
+    return <p>service</p>
 }
-// eslint-disable-next-line
+
 const serviceWrapper = {
     width: "100%"
 }
--- src/sections/Team.js
+++ src/sections/Team.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 export default function Team(props) {
-    // return <img style={teamWrapper} src={require("../assets/Team.svg")} alt=""></img>
-    // return <embed style={teamWrapper} src={require("../assets/Team.svg")} type="image/svg+xml">
-    // </embed>
     return <React.Fragment>
         <div style={style_Navigation_Shadow}></div>
         <embed style={teamWrapper} src={require("../assets/Team.svg")} type="image/svg+xml"></embed>
@@ -13,7 +10,7 @@ export default function Team(props) {
 const style_Navigation_Shadow = {
     height: "96px"
 }
-// eslint-disable-next-line
+
 const teamWrapper = {
     width: "100%"
 }
