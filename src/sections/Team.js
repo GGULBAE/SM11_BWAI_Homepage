c9f245ac11cac06485da6e63a6d42a4a117afeab
GGULBAE <hyeon5727@gmail.com>
2020-10-10 00:07:16 +0900

    [Re:]Team NotoSans

--- src/sections/Team.js
+++ src/sections/Team.js
@@ -4,9 +4,15 @@ export default function Team(props) {
     // return <img style={teamWrapper} src={require("../assets/Team.svg")} alt=""></img>
     // return <embed style={teamWrapper} src={require("../assets/Team.svg")} type="image/svg+xml">
     // </embed>
-    return <span>team</span>
+    return <React.Fragment>
+        <div style={style_Navigation_Shadow}></div>
+        <embed style={teamWrapper} src={require("../assets/Team.svg")} type="image/svg+xml"></embed>
+    </React.Fragment>
 }
 
+const style_Navigation_Shadow = {
+    height: "96px"
+}
 // eslint-disable-next-line
 const teamWrapper = {
     width: "100%"
