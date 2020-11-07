9eea717c3fae28abc5d1ce5411121854633117de
GGULBAE <hyeon5727@gmail.com>
2020-11-08 00:14:07 +0900

    Today Usages

--- src/components/Dashboard/Statistics/Statistics.js
+++ src/components/Dashboard/Statistics/Statistics.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import TodayUsages from './TodayUsages.js';
 // import TodayBilling from './TodayBilling.js';
 import APIUsages from './APIUsages.js';
+import Axios from 'axios';
+import getConfig from '../getConfig.js';
 
 // 당일 API 사용량 + 총 Billing 요금
 export default function Statistics({ apiKey }) {
+  const defaultUsages = {
+    Judge: {
+      prev: 0,
+      today: 0
+    },
+    Masking: {
+      prev: 0,
+      today: 0
+    },
+    Probability: {
+      prev: 0,
+      today: 0
+    },
+    Price: {
+      prev: 0,
+      today: 0
+    }
+  }
+  const [usages, setUsages] = useState(defaultUsages);
+
+  useEffect(() => {
+    async function getPrevTodayData(apiKey) {
+      var Prev = await getPrevData(apiKey);
+      var Today = await getTodayData(apiKey);
+
+      console.group("USEFFECT");
+      console.log(Prev);
+      console.log(Today);
+      console.groupEnd();
+      console.log(setUsages);
+    }
+
+    getPrevTodayData(apiKey);
+  }, [apiKey])
   return <StatisticsWrapper>
     <TodayUsagesWrapper>
-      <TodayUsages/>
-      <TodayUsages/>
-      <TodayUsages/>
-      <TodayUsages/>
+      <TodayUsages type="Judge" prev={usages.Judge.prev} current={usages.Judge.today}/>
+      <TodayUsages type="Masking" prev={usages.Masking.prev} current={usages.Masking.today}/>
+      <TodayUsages type="Probability" prev={usages.Probability.prev} current={usages.Probability.today}/>
+      <TodayUsages type="Price" prev={usages.Price.prev} current={usages.Price.today}/>
     </TodayUsagesWrapper>
     <APIUsages apiKey={apiKey}></APIUsages>
   </StatisticsWrapper>
@@ -30,3 +66,42 @@ const TodayUsagesWrapper = styled.div`
     margin-right: 0;
   }
 `
+
+const getNowDate = () => {
+  const now = new Date();
+
+  return {
+    year: now.getFullYear(),
+    month: now.getMonth() + 1,
+    date: now.getDate()
+  }
+}
+
+const getYesterDayDate = () => {
+  const now = new Date();
+  now.setDate(now.getDate() - 1);
+
+  return {
+    year: now.getFullYear(),
+    month: now.getMonth() + 1,
+    date: now.getDate()
+  }
+}
+
+const apiServer = process.env.REACT_APP_API_SERVER;
+
+const getPrevData = (apiKey) => {
+  const date = getYesterDayDate();
+  const url = `${apiServer}/api/bwai/v1/dashboard/year/${date.year}/month/${date.month}/day/${date.date}`;
+  const config = getConfig(apiKey);
+
+  return Axios.get(url, config)
+}
+
+const getTodayData = (apiKey) => {
+  const date = getNowDate();
+  const url = `${apiServer}/api/bwai/v1/dashboard/year/${date.year}/month/${date.month}/day/${date.date}`;
+  const config = getConfig(apiKey);
+
+  return Axios.get(url, config)
+}
--- src/components/Dashboard/Statistics/TodayUsages.js
+++ src/components/Dashboard/Statistics/TodayUsages.js
@@ -1,21 +1,91 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function TodayUsages({ apiKey }) {
+const getRate = (prev, current) => {
+  const rate = current / prev;
+  
+  return rate;
+}
+
+export default function TodayUsages({ apiKey, type, prev, current }) {
+  const rate = getRate(prev, current);
+
   return <TodayUsagesWrapper>
     <TitleWrapper>
-      <Title>Today</Title>
+      <Title>
+        당일 사용량 
+        <TitleType>({type})</TitleType>
+      </Title>
     </TitleWrapper>
+    <ContentsWrapper>
+      <CurrentData>
+        {current}회
+      </CurrentData>
+      <CompareToPrev>
+        <Arrow rate={rate}></Arrow>
+        <RateWrapper className={rate > 1 ? "up" : "down"}>{rate * 100}%</RateWrapper>
+        <RateDate>since yesterday</RateDate>
+      </CompareToPrev>
+    </ContentsWrapper>
   </TodayUsagesWrapper>
 }
 
+const Arrow = ({rate}) => {
+  const ArrowWrapper = styled.div`
+    display: inline-block;
+    height: 24px;
+  `
+
+  return <ArrowWrapper>
+    { rate > 1 ? 
+    <UP_SVG class="MuiSvgIcon-root jss25" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"></path>
+    </UP_SVG> : 
+    <DOWN_SVG class="MuiSvgIcon-root jss20" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"></path>
+    </DOWN_SVG>
+    }
+  </ArrowWrapper>
+}
+
+const UP_SVG = styled.svg`
+  -webkit-font-smoothing: antialiased;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  fill: currentColor;
+  width: 1em;
+  height: 1em;
+  display: inline-block;
+  font-size: 1.5rem;
+  transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  flex-shrink: 0;
+  user-select: none;
+  color: #1b5e20;
+`
+const DOWN_SVG = styled.svg`
+  -webkit-font-smoothing: antialiased;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  fill: currentColor;
+  width: 1em;
+  height: 1em;
+  display: inline-block;
+  font-size: 1.5rem;
+  transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  flex-shrink: 0;
+  user-select: none;
+  color: #b71c1c;
+`
+
 const TodayUsagesWrapper = styled.div`
   margin-right: 24px;
-  // width: calc(100% - 36px);
   border-radius: 5px 5px;
   box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
   background-color: #FFF;
-  overflow-x: scroll;
+  // overflow-x: scroll;
+  overflow: hidden;
 `
 
 const TitleWrapper = styled.div`
@@ -25,7 +95,40 @@ const TitleWrapper = styled.div`
 
 const Title = styled.p`
   margin: 0;
-  display: inline-block;
   font-weight: bold;
-  width: calc(100% - 156px);
+`
+
+const TitleType = styled.span`
+  font-weight: normal;
+  color: rgba(0, 0, 0, 0.6);
+`
+
+const ContentsWrapper = styled.div`
+  padding: 16px;
+  text-align: right;
+`
+
+const CurrentData = styled.p`
+  font-size: 24px;
+  margin-top: 12px;
+  margin-bottom: 0;
+`
+const CompareToPrev = styled.p`
+  margin: 0;
+`
+
+const RateWrapper = styled.span`
+  font-size: 12px;
+  &.up {
+    color: #1b5e20;
+  }
+
+  &.down {
+    color: #b71c1c;
+  }
+`
+const RateDate = styled.p`
+  font-size:12px;
+  color: gray;
+  margin: 0;
 `
