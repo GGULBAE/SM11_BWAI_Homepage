78d282e80dbc7b504e6ce021d45dc350debe8447
GGULBAE <hyeon5727@gmail.com>
2020-11-17 22:47:52 +0900

    [Updated]DashBoard

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,42 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import Axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+// import Axios from 'axios';
+import React from 'react';
 
-import Header from './Dashboard/Header.js';
-import MainFrame from './Dashboard/MainFrame.js';
+// import Header from './Dashboard/Header.js';
+// import MainFrame from './Dashboard/MainFrame.js';
 
-const apiServer = process.env.REACT_APP_API_SERVER;
+// const apiServer = process.env.REACT_APP_API_SERVER;
 
 export default function DashBoard() {
-  const [myKey, setMyKey] = useState(window.sessionStorage.getItem("myAPIKey"));
+  window.location.href = "http://dashboard.bwai.io/app/dashboard";
 
-  useEffect(() => {
-    if (myKey === null) {
-      var id = window.prompt("아이디 입력")
-      var pw = window.prompt("비밀번호 입력")
+  return <p>Redirectiong...</p>
+  // const [myKey, setMyKey] = useState(window.sessionStorage.getItem("myAPIKey"));
 
-      var url = `${apiServer}/api/auth/signin`;
-      var form = {
-        "id": id,
-        "pw": pw
-      }
+  // useEffect(() => {
+  //   if (myKey === null) {
+  //     var id = window.prompt("아이디 입력")
+  //     var pw = window.prompt("비밀번호 입력")
 
-      Axios.post(url, form)
-        .then((res) => {
-          console.log(res);
-          var result = res.data.result;
-          if (result) {
-            window.sessionStorage.setItem("myAPIKey", result.user_token);
-            setMyKey(result.user_token);
-            window.location.reload();
-          } else {
-            alert("로그인 실패");
-          }
-        })
-    }
-  }, [myKey])
+  //     var url = `${apiServer}/api/auth/signin`;
+  //     var form = {
+  //       "id": id,
+  //       "pw": pw
+  //     }
 
-  return <React.Fragment>
-    <Header />
-    {myKey ? <MainFrame apiKey={myKey}/> : null}
-  </React.Fragment>
+  //     Axios.post(url, form)
+  //       .then((res) => {
+  //         console.log(res);
+  //         var result = res.data.result;
+  //         if (result) {
+  //           window.sessionStorage.setItem("myAPIKey", result.user_token);
+  //           setMyKey(result.user_token);
+  //           window.location.reload();
+  //         } else {
+  //           alert("로그인 실패");
+  //         }
+  //       })
+  //   }
+  // }, [myKey])
+
+  // return <React.Fragment>
+  //   <Header />
+  //   {myKey ? <MainFrame apiKey={myKey}/> : null}
+  // </React.Fragment>
 }
